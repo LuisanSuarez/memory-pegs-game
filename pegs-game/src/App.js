c4044c1be5a8cc709f95834d2107178f520e2c41
Luisan <luisangelsc@gmail.com>
2020-01-13 16:53:47 -0600

    game MVP show question, options, correct answer. Get new question after getting an answer right

---
+++ pegs-game/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import QuestionCard from './components/QuestionCard'
+import ComboTracker from './components/ComboTracker'
+import Options from './components/Options'
+
+
+const Game = () => {
+    const [optionsAmount, setOptionsAmount] = useState(4);
+    const [answer, setAnswer] = useState(72);
+    const [userAnswer, setUserAnswer] = useState(null);
+    const [newOptions, setNewOptions] = useState(true)
+    //on user input
+        //setOptionsAmount
+    const sendAnswer = userAnswer => {
+        console.log("answer:", userAnswer);
+        setUserAnswer(userAnswer);
+    }
+
+    const nextCard = () => {
+        setUserAnswer(null);
+        setNewOptions(true);
+        setAnswer(Math.round(Math.random()*110))
+        console.log("App:", newOptions)
+    }
+
+    return (
+        <>
+            <h1>Memory Game</h1>
+            <QuestionCard
+                questionNumber={answer}
+                userAnswer={userAnswer}
+                nextCard={nextCard}
+            />
+            <ComboTracker />
+            <Options
+                optionsAmount={optionsAmount}
+                answer={answer}
+                sendAnswer={sendAnswer}
+                newOptions={newOptions}
+                setNewOptions={setNewOptions}
+            />
+        </>
+        )
+}
+
+export default Game
