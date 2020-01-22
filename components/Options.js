642439b26f25a97a95672f61bb2f8b921e4be0b0
Luisan <luisangelsc@gmail.com>
2020-01-22 15:12:19 -0600

    remove .next files from repo

--- components/Options.js
+++ components/Options.js
@@ -1,50 +1,52 @@
-import React, {useEffect, useState} from 'react';
-import Option from './Option'
-import ImageUpload from './ImageUpload'
+import React, { useEffect, useState } from "react";
+import Option from "./Option";
+import ImageUpload from "./ImageUpload";
 
 //TODO: destructure here
-const Options = ({...props}) => {
-    const {optionsAmount, answer, sendAnswer, setNewOptions} = props;
-    let {newOptions} = props;
-    const [options, setOptions] = useState([]);
+const Options = ({ ...props }) => {
+  const { optionsAmount, answer, sendAnswer, setNewOptions } = props;
+  const { newOptions } = props;
+  const [options, setOptions] = useState([]);
 
-    useEffect(() => {
-        console.log("newOptions:", newOptions);
-        newOptions ? setOptions(createOptions(optionsAmount)) : '';
-        setNewOptions(false);
-    }, [newOptions])
+  useEffect(() => {
+    console.log("newOptions:", newOptions);
+    newOptions ? setOptions(createOptions(optionsAmount)) : "";
+    setNewOptions(false);
+  }, [newOptions]);
 
-    const shuffleArray = (array) => {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
+  const shuffleArray = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
+  };
 
-    const createOptions = optionsAmount => {
-        console.log("createOptions runs because newOptions is:", newOptions);
-        let options = [];
-        let randomNumber = Math.round(Math.random()*110);
-        for (let i=0; i<optionsAmount-1; i++) {
-            do {
-                randomNumber = Math.round(Math.random()*110);
-            } while (randomNumber === answer)
+  const createOptions = optionsAmount => {
+    console.log("createOptions runs because newOptions is:", newOptions);
+    let options = [];
+    let randomNumber = Math.round(Math.random() * 110);
+    for (let i = 0; i < optionsAmount - 1; i++) {
+      do {
+        randomNumber = Math.round(Math.random() * 110);
+      } while (randomNumber === answer);
 
-            options.push(<Option id={randomNumber} answer={randomNumber} sendAnswer={sendAnswer}/>)
-        }
-        options.push(<Option id={answer} answer={answer} sendAnswer={sendAnswer}/>)
-        shuffleArray(options)
-        //shuffle options order
-        return options
+      options.push(
+        <Option
+          id={randomNumber}
+          answer={randomNumber}
+          sendAnswer={sendAnswer}
+        />
+      );
     }
-    console.log("OPTIONS is Rendering");
+    options.push(
+      <Option id={answer} answer={answer} sendAnswer={sendAnswer} />
+    );
+    shuffleArray(options);
+    //shuffle options order
+    return options;
+  };
 
-    return (
-        <>
-            {/* <ImageUpload pegNumber={3} /> */}
-            {options}
-        </>
-    )
-}
+  return <>{options}</>;
+};
 
-export default Options
+export default Options;
