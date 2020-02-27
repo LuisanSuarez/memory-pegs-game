d320a123bcb15efb28b1dc8e8e9f53c66e17b45f
Luisan <luisangelsc@gmail.com>
2020-02-27 10:54:48 -0600

    now crashed - debug attempt #2

--- globalVariables.js
+++ globalVariables.js
@@ -1,5 +1,5 @@
-const productionUrlServer = "https://memory-pegs-backend.herokuapp.com/";
-// const productionUrlServer = "http://localhost:8000/";
+// const productionUrlServer = "https://memory-pegs-backend.herokuapp.com/";
+const productionUrlServer = "http://localhost:8000/";
 const devUrlServer = "http://localhost:8000/";
 const productionLoginUri = "https://memory-pegs-game.luisangelsc.now.sh";
 const devLoginUri = "http://localhost:3000";
