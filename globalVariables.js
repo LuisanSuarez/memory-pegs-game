4c4d9b6cf43213a87a6827ba09421e7a47e2bd8e
Luisan <luisangelsc@gmail.com>
2020-02-28 16:07:31 -0600

    now crashed - debug attempt #9

--- globalVariables.js
+++ globalVariables.js
@@ -1,7 +1,7 @@
 const productionUrlServer = "https://memory-pegs-backend.herokuapp.com/";
 // const productionUrlServer = "http://localhost:8000/";
 const devUrlServer = "http://localhost:8000/";
-const productionLoginUri = "https://memory-pegs-game.luisangelsc.now.sh";
+const productionLoginUri = "https://memory-pegs-game.luisangelsc.now.sh/";
 const devLoginUri = "http://localhost:3000";
 
 const random = "shit for commit";
